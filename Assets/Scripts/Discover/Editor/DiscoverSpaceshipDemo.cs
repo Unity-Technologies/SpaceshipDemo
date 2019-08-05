0b0e4b16e17d3780de075eda38f7c9a8d870d6dd
Thomas ICHÉ <peeweek@gmail.com>
2019-08-05 15:25:51 +0200

    Fixed First Import Errors + Warnings (p4#4388)

--- Assets/Scripts/Discover/Editor/DiscoverSpaceshipDemo.cs
+++ Assets/Scripts/Discover/Editor/DiscoverSpaceshipDemo.cs
@@ -3,19 +3,12 @@ using System.Collections.Generic;
 using UnityEditor;
 using GameplayIngredients.Editor;
 
-[InitializeOnLoad]
 static class DiscoverSpaceshipDemo
 {
-    static DiscoverAsset asset;
-
-    static DiscoverSpaceshipDemo()
-    {
-        asset = AssetDatabase.LoadAssetAtPath<DiscoverAsset>("Assets/Scripts/Discover/DiscoverSpaceshipDemo.asset");
-    }
-
     [MenuItem("Help/Discover Spaceship Demo", priority = 0)]
     static void ShowSpaceshipDiscover()
     {
+        var asset = AssetDatabase.LoadAssetAtPath<DiscoverAsset>("Assets/Scripts/Discover/DiscoverSpaceshipDemo.asset");
         DiscoverWindow.ShowDiscoverWindow(asset);
     }
 }
